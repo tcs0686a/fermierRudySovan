9b79d998ab03d98ffe2caf25a566ed50b2a5fa4e
tcs0686a <touch.sovannroth@gmail.com>
2022-02-25 10:13:31 +0100

    first fermier

---
+++ src/main/java/com/example/demo/FermierApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FermierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FermierApplication.class, args);
+	}
+
+}
